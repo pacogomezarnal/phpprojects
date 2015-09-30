c4b570c656409c96036b0b012723a878b630cd56
Paco Gomez Arnal <paco.gomez.arnal@gmail.com>
2015-09-30 22:41:27 +0200

    Actualizado codigo de control de flujo

---
+++ flujo/condicional_corto.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>CONTROL DE FLUJO CORTO
+		</title>
+		<style>
+		.OK {
+			width:50%;
+			border: 1px solid #000000;
+			margin:auto;
+		    background-color: #00E676;
+		}
+		.KO {
+			width:50%;
+			border: 1px solid #000000;
+			margin:auto;
+		    background-color: #FF3D00;
+		}
+		</style>
+	</head>
+	<body>
+		<?php
+		$num_aleatorio=rand(0,10);
+		?>
+		<div class="<?=($num_aleatorio>5?'OK':'KO')?>">El numero ha sido <?=$num_aleatorio?></div>
+		<div class="<?=($num_aleatorio>5?'OK':'KO')?>">El numero ha sido <?=$num_aleatorio?></div>
+		<div class="<?=($num_aleatorio>5?'OK':'KO')?>">El numero ha sido <?=$num_aleatorio?></div>
+	</body>
+</html>
